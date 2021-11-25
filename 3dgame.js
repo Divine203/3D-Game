68dcf745d4dd4b829ff3ddfe51b08ac0d4f1bf76
Divine203 <85737884+Divine203@users.noreply.github.com>
2021-11-25 06:15:24 -0800

    Add files via upload

---
+++ 3dgame.js
@@ -0,0 +1,53 @@
+function moveLeft(){
+	let left = 
+	parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+	left -= 100;
+	if(left>=0){
+		character.style.left = left + "px";
+		character.style.transition = "left .1s ease-in 0s";
+	}
+}
+
+function moveRight(){
+	let left = 
+	parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+	left += 100;
+	if(left<300){
+		character.style.left = left + "px";
+		character.style.transition = "left .1s ease-in 0s";
+	}
+}
+
+document.addEventListener("keydown", event => {
+	if(event.key==="ArrowLeft"){
+		moveLeft();
+	}
+	if(event.key==="ArrowRight"){
+		moveRight();
+	}
+});
+
+var counter = 0;
+var block = document.getElementById("block");
+block.addEventListener('animationiteration', ()=>{
+	var random = Math.floor(Math.random() * 3);
+	left = random * 100;
+	block.style.left = left + "px";
+	counter++;
+});
+
+setInterval(function(){
+	var characterLeft = parseInt(window.getComputedStyle(character)
+		.getPropertyValue("left"));
+	var blockLeft = parseInt(window.getComputedStyle(block)
+		.getPropertyValue("left"));
+	var blockTop = parseInt(window.getComputedStyle(block)
+		.getPropertyValue("top"));
+
+	if(characterLeft == blockLeft && blockTop<500 && blockTop>300){
+		alert("SCORE:"+counter);
+		counter = 0;
+		block.style.animation = "none";
+	}
+}, 1);
+
